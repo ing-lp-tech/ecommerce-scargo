af07ee6c6a0ff92e38588174d72ad4780638d9f2
ing lp <ing.lp.tech@gmail.com>
2023-09-21 13:45:41 -0300

    feat:start proyect

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* import reactLogo from "./assets/react.svg"; */
+/* import viteLogo from "/vite.svg"; */
+import ChildCareIcon from "@mui/icons-material/ChildCare";
+
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button>
+        click <ChildCareIcon />
+      </Button>
+    </>
+  );
+}
+
+export default App;
